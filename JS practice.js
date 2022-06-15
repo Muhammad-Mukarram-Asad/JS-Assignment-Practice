0c8c9a3f89fc44998284bb78da5c19c5081a8503
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2022-06-15 12:21:43 +0500

    Add files via upload

---
+++ JS practice.js
@@ -0,0 +1,43 @@
+var f = prompt(" Enter first name:");
+var l = prompt(" Enter last name:");
+var full = f + l;
+console.log(full);
+
+// Question: 01 , 02  >>> Done
+
+// first method
+var str = "Ali and babar are best friends and they both play cricket.";
+var str_array = str.split(" ");
+console.log(str_array);
+
+for(var i = 0 ; i<str_array.length ; i++)
+{
+    if(str_array[i] == "and")
+    {
+        str_array[i] = "&";
+    }
+}
+str = str_array.join(" ");
+console.log(str);
+
+// second method
+var str = "Ali and babar are best friends and they both play cricket.";
+console.log(str.replaceAll("and" , "&"));
+
+// third method
+var str = "Ali and babar are best friends and they both play cricket.";
+console.log(str);
+
+for(var i = 0 ; i<str.length ; i++)
+{
+    // i = 4
+    if(str.slice(i, i+3) == "and")
+    {
+        var first_part = str.slice(0,i); // (0,4)
+        var new_text = "&";
+        var last_part = str.slice(i+3); // (d ka baad sa lena hai)
+        
+        str = first_part + new_text + last_part;
+    }
+}
+console.log(str);
